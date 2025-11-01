e2492a45979a18572d20d23672a5fbe52ab30b6a
admin_zooq32 <h9a0fia0@gmail.com>
2025-11-01 18:32:11 +0700

    version 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.starvpn.proxy.nightproxy.knight"
         minSdk = 24
         targetSdk = 36
-        versionCode = 102
-        versionName = "1.0.2"
+        versionCode = 103
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         setProperty("archivesBaseName", "${rootProject.name}-v${versionName}-${versionCode}")
